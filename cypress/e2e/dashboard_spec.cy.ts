e813918cb757b471f6ea164f089b2244ecb10438
Tayla Phillips <76406423+tednaphil@users.noreply.github.com>
2024-04-05 21:52:22 -0500

    Add assertions for new features

--- cypress/e2e/dashboard_spec.cy.ts
+++ cypress/e2e/dashboard_spec.cy.ts
@@ -6,6 +6,7 @@ describe('Spilled', () => {
     }).as('getTeas')
     .visit('http://localhost:3000/')
   })
+
   it('Displays landing page', () => {
     cy.get('h1').contains('Spilled')
     .get('nav').children().should('have.length', 3)
@@ -16,34 +17,64 @@ describe('Spilled', () => {
     .get('.div-nav-center').contains('a', 'Oolong')
     .get('.div-nav-center').contains('a', 'White')
     .get('img').should('have.attr', 'src').should('include', 'data:image')
+    .get('article').first().within(() => {
+      cy.get('h2').contains('Home')
+      .get('p').contains('Lorem ipsum')
+      .get('img').should('have.attr', 'src').should('include', '/static/media/teas')
+    })
+    .get('article').last().within(() => {
+      cy.get('h2').contains('Blends')
+      .get('p').contains('Lorem ipsum')
+      .get('img').should('have.attr', 'src').should('include', '/static/media/teas')
+    })
     //test main secion content - we need to add classes to some elements
     //to make them easier to access    
   })
+
   it('Displays tea pages from nav bar', () => {
     cy.get('nav').contains('a', 'Blends').click()
-    // .url().should('eq', '')
-    //check url
+    .url().should('include', 'blend')
+    //check content on page
     .get('nav').contains('a', 'White').click()
-    //check url
+    .url().should('include', 'white')
+    //check content on page
     .get('.nav-favorites').click()
-    //check url
-    //add assertion to test empty favorites page
+    .url().should('include', 'favorites')
+    //check that empty favorites page displays message
 
   })
+
   it('Displays tea pages from info article buttons', () => {
     cy.get('.article-tea').first().contains('button', 'See Black teas →').click()
     .get('h1').click()
     .get('.article-tea').last().contains('button', 'See tea blends →').click()
   })
+
   it('Adds and removes teas from Favorites list', () => {
-    //simulate click of a fav button
-    //click favs nav button
-    //check for added fav
-    //simulate fav button click
-    //check that favs page is empty
-  })
-  it('Displays error message if api call fails', () => {
+    cy.get('nav').contains('a', 'White').click()
+    .get('div.card').first().within(() => {
+      cy.get('h3').contains('Baihoi Yinzhen')
+      .get('button').contains('Fav').click()
+    })
+    //add assertions for new tea details
+    cy.get('.nav-favorites').click()
+    .url().should('include', 'favorites')
+    .get('div.card').within(() => {
+      cy.get('h3').contains('Baihoi Yinzhen')
+      .get('button').click()
+    }).should('not.exist')
+    //check that favs page is empty and displays message
     
   })
+//   it('Displays error message if api call fails', () => {
+//     cy.intercept('GET', 'https://boonakitea.cyclic.app/api/all', {
+//       statusCode: 400,
+//     }).as('getTeas')
+//     .visit('http://localhost:3000/')
+//     .get('nav').contains('a', 'Blends').click()
+//     // test error page content
+//     //stub 500 statusCode
+//     //test error page content
+//   })
 
 })
