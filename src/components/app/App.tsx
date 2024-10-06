902082a6543880a4f9366c9a596f62211b3fea57
Tayla Phillips <76406423+tednaphil@users.noreply.github.com>
2024-10-06 13:16:32 -0500

    Remove path display and rules

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -4,17 +4,13 @@ import Home from "../home/Home";
 import Teas from "../teas/Teas";
 import Error from "../error/Error";
 import TeaEd from "../teaEd/TeaEd";
-import { Routes, Route, useLocation } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  const location = useLocation().pathname;
 
   return (
     <div className="App">
       <Nav />
-      <div id="location-path-cont">
-        <p id="location-path">{location === "/" ? "/home" : location}</p>
-      </div>
       <main>
         <Routes>
           <Route path="/" element={<Home />} />
