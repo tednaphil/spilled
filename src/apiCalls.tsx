c6b79abb46b3c1deaca37b386a1bbc4802d9ec79
Tayla Phillips <76406423+tednaphil@users.noreply.github.com>
2024-10-06 18:55:27 -0500

    Add postUser declaration

--- src/apiCalls.tsx
+++ src/apiCalls.tsx
@@ -22,4 +22,34 @@ const fetchSingleTea = async (id: number) => {
   }
 };
 
-export { fetchTea, fetchSingleTea };
+const postUser = async (username: string) => {
+    try {
+        const response = await fetch("https://spilled-api.onrender.com/users",
+            {
+                method: "POST",
+                body: JSON.stringify({user: {
+                    username: "Tony"
+                    }
+                })
+            }
+        );
+        if(!response.ok) {
+            throw new Error("Couldn't create user");
+        }
+        console.log(response.json())
+        // return await response.json()
+    } catch (error: unknown) {
+        throw error
+    }
+
+};
+
+const postFav = async (userId: number, teaId: number) => {
+
+};
+
+const deleteFav = async (favId: number) => {
+
+};
+
+export { fetchTea, fetchSingleTea, postUser };
